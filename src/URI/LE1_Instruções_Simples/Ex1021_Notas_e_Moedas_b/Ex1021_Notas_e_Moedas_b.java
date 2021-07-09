e0f1702e7c340fcccd35d4143635afe0af3db52d
klesio25 <klesio.k9@gmail.com>
2021-07-09 20:53:18 -0300

    Ballot problem solved ex1021

--- src/URI/LE1_Instruções_Simples/Ex1021_Notas_e_Moedas_b/Ex1021_Notas_e_Moedas_b.java
+++ src/URI/LE1_Instruções_Simples/Ex1021_Notas_e_Moedas_b/Ex1021_Notas_e_Moedas_b.java
@@ -2,12 +2,10 @@ package URI.LE1_Instruções_Simples.Ex1021_Notas_e_Moedas_b;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Arrays;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Ex1021_Notas_e_Moedas_b{
+public class Ex1021_Notas_e_Moedas_b {
     public static void main(String[] args) throws IOException {
         Locale.setDefault(Locale.US);
         InterfaceTexto dinheiro = new InterfaceTexto();
@@ -34,47 +32,61 @@ class InterfaceTexto {
     }
 
     private void saindaDados() {
-        List<Integer> cedulas = Arrays.asList(100, 50, 20, 10, 5, 2);
 
         System.out.printf("NOTAS: \n");
-        for (Integer nota : dinheiro.getQuantidadeNotas()) {
-            System.out.printf("%d nota(s) de ", nota);
-            for (Integer cedula : cedulas) {
-                System.out.printf("R$ %d,00\n", cedula);
-            }
+        for (Monetario nota : dinheiro.getQuantidadeNotas()) {
+            System.out.printf("%d nota(s) de R$ %d,00\n", nota.getQuantidadeNota(), nota.getCedula());
         }
-
 
     }
 }
 
 class Monetario {
+    private int quantidadeNota;
+    private int quantidadeMoedas;
+    private int cedula;
 
+    public Monetario(int quantidadeNota, int cedula) {
+        this.quantidadeNota = quantidadeNota;
+        this.cedula = cedula;
+    }
 
+    public int getCedula() {
+        return cedula;
+    }
+
+    public int getQuantidadeNota() {
+        return quantidadeNota;
+    }
+
+    public int getQuantidadeMoedas() {
+        return quantidadeMoedas;
+    }
 }
 
 
 class ContadorDinheiro {
     private double valor;
-    private ArrayList<Integer> quantidadeNotas = new ArrayList<>();
-    private ArrayList<Integer> quantidadeMoedas = new ArrayList<>();
+    private ArrayList<Monetario> quantidadeNotas = new ArrayList<>();
+    private ArrayList<Monetario> quantidadeMoedas = new ArrayList<>();
     private int[] cedulas = {100, 50, 20, 10, 5, 2};
 
     public void setValor(double valor) {
         this.valor = valor;
     }
 
-    public ArrayList<Integer> getQuantidadeNotas() {
+    public ArrayList<Monetario> getQuantidadeNotas() {
         for (int i = 0; i < cedulas.length; i++) {
             int nota = 0;
             nota = (int) valor / cedulas[i];
+            quantidadeNotas.add(new Monetario(nota, cedulas[i]));
             valor = valor % cedulas[i];
-            quantidadeNotas.add(nota);
+
         }
         return quantidadeNotas;
     }
 
-    public ArrayList<Integer> getQuantidadeMoedas() {
+   /* public ArrayList<Integer> getQuantidadeMoedas() {
         int centavos = (int) valor;
         int moedas = (int) ((valor - centavos) * 100);
 
@@ -87,6 +99,6 @@ class ContadorDinheiro {
         }
 
         return quantidadeMoedas;
-    }
+    }*/
 
 }
